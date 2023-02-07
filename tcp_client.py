ced8e3a3b534eb1e071b328b98204f29b413cb03
Michelle Arredondo <mearredo@usc.edu>
2023-02-06 22:59:42 -0800

    Update tcp_client.py

--- tcp_client.py
+++ tcp_client.py
@@ -9,8 +9,25 @@ import socket
 
 def main():
     # TODO: Create a socket and connect it to the server at the designated IP and port
+    HOST = "172.20.10.10" #server's hostname or IP address (use IP address of RPi when using rpi: 172.20.10.10) (localhost is used when just testing with your own device)
+    PORT = 8000 #port used by the server (10000 or 8000 works)
+    
+    #with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    
+    
     # TODO: Get user input and send it to the server using your TCP socket
+    inp = input('Message:\n')
+    b_inp= bytes(inp, 'utf-8') #convert string into bytes 
+ 
+    s.sendall (b_inp)
+    
     # TODO: Receive a response from the server and close the TCP connection
+    
+    data = s.recv(1024).decode() #socket is receiving data in a buffer size of 1024 bytes at a time #decode reverses the encryption from the server
+    print(f"Received {data!r}")
+    s.close()
     pass
 
 
